730b82974f807483758cf01687f2050747534cf1
Inan Yigit <inanygt@hotmail.com>
2022-12-02 15:18:35 +0100

    added second  getDetails api

--- script.js
+++ script.js
@@ -22,16 +22,25 @@ search.addEventListener("click", function () {
          let output = document.getElementById("output");
 
          data.Search.forEach((element) => {
-            output.innerHTML += `<div class="col-3 ">
+            output.innerHTML += `<div class="col-md ">
             <div class="bg-dark rounded-3"> 
             <div class="title"> ${element.Title} </div>
             
             <div class="year">   ${element.Year} </div>
             <img src='${element.Poster}' width="100%"> <br>
-            <button class="btn btn-light m-3">Details</button> <br>
+            <button class="btn btn-light m-3" onClick="getDetails('${element.imdbID}')">Details</button> <br>
             </div>
             </div>`;
+
+         
          });
-      })
-      .catch((error) => console.log(error));
+      }).catch((error) => console.log(error));
 });
+
+function getDetails(id) {
+   fetch('http://www.omdbapi.com/?i=' + id + '&apikey=bfc0d76f').then(
+   res => res.json()
+   ).then(
+      data => console.log(data)
+   ).catch();
+}
