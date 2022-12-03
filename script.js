7441e62fa11e7b6282d401d35df483e738039e8c
Inan Yigit <inanygt@hotmail.com>
2022-12-03 11:54:44 +0100

    getting details from getDetails button

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ let title = document.getElementById("title");
 let releaseDate = document.getElementById("releaseDate");
 let poster = document.getElementById("poster");
 
+let modal = document.getElementById("modal-details");
+
 search.addEventListener("click", function () {
    let input = document.getElementById("input").value;
    let url = "http://www.omdbapi.com/?s=" + input + "&apikey=bfc0d76f";
@@ -26,7 +28,7 @@ search.addEventListener("click", function () {
          output.innerHTML = " ";
          data.Search.forEach((element) => {
             
-            output.innerHTML += `<div class="col-lg-3 col-md-4 col-sm-6 ">
+            output.innerHTML += `<div class="col-lg-3 col-md-4 col-sm-6">
             <div class="box bg-dark rounded-3"> 
             <div class="title"> ${element.Title} </div>
             
@@ -35,18 +37,38 @@ search.addEventListener("click", function () {
             <button class="btn btn-light m-3 btn-details" onClick="getDetails('${element.imdbID}')">Details</button> <br>
             </div>
             </div>`;
-
-         
          });
       }).catch((error) => console.log(error));
 });
+
+
 
 function getDetails(id) {
    fetch('http://www.omdbapi.com/?i=' + id + '&apikey=bfc0d76f')
    .then(
    res => res.json()
    ).then(
-      data => console.log(data)
+      data => {
+         console.log(data)
+         modal.innerHTML = `<div class="row">
+            <div class="col">
+               <img src='${data.Poster}'"> 
+            </div>
+            <div class="col">
+         ${data.Title} <br>
+         ${data.Year} <br>
+         ${data.Genre} <br>
+         <i class="fa-solid fa-star"></i> ${data.imdbRating} <br>
+         ${data.Actors}
+         </div>
+         <div class="row">
+            <div class="col">
+                 ${data.Plot}
+            </div>
+         </div>
+         </div>`;
+
+      } 
    ).catch(
       (error) => console.log(error)
    );
