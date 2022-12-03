2bb6b5b2bc42fdf75c9a7851806ea64c881af789
Inan Yigit <inanygt@hotmail.com>
2022-12-03 13:55:28 +0100

    added modal functionality

--- script.js
+++ script.js
@@ -2,12 +2,11 @@ let search = document.getElementById("search");
 let style = document.getElementById("style");
 let input = document.getElementById("input");
 
-input.addEventListener("keypress", function(event) {
-  // If the user presses the "Enter" key on the keyboard
-  if (event.key === "Enter") {
-      
-   document.getElementById("search").click();
-  }
+input.addEventListener("keypress", function (event) {
+   // If the user presses the "Enter" key on the keyboard
+   if (event.key === "Enter") {
+      document.getElementById("search").click();
+   }
 });
 
 let title = document.getElementById("title");
@@ -15,6 +14,9 @@ let releaseDate = document.getElementById("releaseDate");
 let poster = document.getElementById("poster");
 
 let modal = document.getElementById("modal-details");
+let modalPoster = document.getElementById("modal-poster");
+let modalInfo = document.getElementById("modal-info");
+let modalFooter = document.getElementById("modal-footer");
 
 search.addEventListener("click", function () {
    let input = document.getElementById("input").value;
@@ -27,49 +29,40 @@ search.addEventListener("click", function () {
          let output = document.getElementById("output");
          output.innerHTML = " ";
          data.Search.forEach((element) => {
-            
             output.innerHTML += `<div class="col-lg-3 col-md-4 col-sm-6">
             <div class="box bg-dark rounded-3"> 
             <div class="title"> ${element.Title} </div>
             
             <div class="year">   ${element.Year} </div>
             <img src='${element.Poster}' width="100%"> <br>
-            <button class="btn btn-light m-3 btn-details" onClick="getDetails('${element.imdbID}')">Details</button> <br>
+            <button data-bs-toggle="modal"
+                  data-bs-target="#reg-modal" class="btn btn-light m-3 btn-details" onClick="getDetails('${element.imdbID}')">Details</button> <br>
             </div>
             </div>`;
          });
-      }).catch((error) => console.log(error));
+      })
+      .catch((error) => console.log(error));
 });
 
-
-
 function getDetails(id) {
-   fetch('http://www.omdbapi.com/?i=' + id + '&apikey=bfc0d76f')
-   .then(
-   res => res.json()
-   ).then(
-      data => {
-         console.log(data)
-         modal.innerHTML = `<div class="row">
+   fetch("http://www.omdbapi.com/?i=" + id + "&apikey=bfc0d76f")
+      .then((res) => res.json())
+      .then((data) => {
+         console.log(data);
+         modalPoster.innerHTML = `<div class="row">
             <div class="col">
                <img src='${data.Poster}'"> 
-            </div>
-            <div class="col">
+            </div>`;
+         modalInfo.innerHTML = `
          ${data.Title} <br>
          ${data.Year} <br>
          ${data.Genre} <br>
          <i class="fa-solid fa-star"></i> ${data.imdbRating} <br>
          ${data.Actors}
-         </div>
-         <div class="row">
-            <div class="col">
-                 ${data.Plot}
-            </div>
-         </div>
-         </div>`;
-
-      } 
-   ).catch(
-      (error) => console.log(error)
-   );
+         `;
+         modalFooter.innerHTML = `
+         ${data.Plot}
+         `;
+      })
+      .catch((error) => console.log(error));
 }
