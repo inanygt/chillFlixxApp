376d15aa5de9f687d510670141201b6876dcc5d8
Inan Yigit <inanygt@hotmail.com>
2022-12-03 09:49:34 +0100

    all cols equal heights

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 let search = document.getElementById("search");
 let style = document.getElementById("style");
-// let currentTime = document.getElementById("currentTime");
-
-// const date = new Date();
-// currentTime.innerHTML = date;
-
-// let output = document.getElementById("output");
-
 let input = document.getElementById("input");
 
-// Execute a function when the user presses a key on the keyboard
 input.addEventListener("keypress", function(event) {
   // If the user presses the "Enter" key on the keyboard
   if (event.key === "Enter") {
@@ -35,12 +27,12 @@ search.addEventListener("click", function () {
          data.Search.forEach((element) => {
             
             output.innerHTML += `<div class="col-lg-3 col-md-4 col-sm-6 ">
-            <div class="bg-dark rounded-3"> 
+            <div class="box bg-dark rounded-3"> 
             <div class="title"> ${element.Title} </div>
             
             <div class="year">   ${element.Year} </div>
             <img src='${element.Poster}' width="100%"> <br>
-            <button class="btn btn-light m-3" onClick="getDetails('${element.imdbID}')">Details</button> <br>
+            <button class="btn btn-light m-3 btn-details" onClick="getDetails('${element.imdbID}')">Details</button> <br>
             </div>
             </div>`;
 
@@ -50,9 +42,12 @@ search.addEventListener("click", function () {
 });
 
 function getDetails(id) {
-   fetch('http://www.omdbapi.com/?i=' + id + '&apikey=bfc0d76f').then(
+   fetch('http://www.omdbapi.com/?i=' + id + '&apikey=bfc0d76f')
+   .then(
    res => res.json()
    ).then(
       data => console.log(data)
-   ).catch();
+   ).catch(
+      (error) => console.log(error)
+   );
 }
